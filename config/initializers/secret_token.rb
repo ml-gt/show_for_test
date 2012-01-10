3dd884ab3a17672b1392e88b5c874b98a8bbf8c7
gt-sdi <gto.sdi@gmail.com>
2012-01-10 15:04:52 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowForTest::Application.config.secret_token = '033e4d39681fb474d875b447cf2f23f402f18298de100f9afb680bfa59bd50a81fe12957bab15f34ec925924a053187f9a62745883ea4ee07c4402a9517e6b8b'
